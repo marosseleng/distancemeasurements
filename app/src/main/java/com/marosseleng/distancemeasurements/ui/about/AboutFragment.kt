520f16d002244dc6505730697e7824a3a0954622
Maroš Šeleng <marosseleng@gmail.com>
2019-05-09 19:01:09 +0200

    Create About page.

--- app/src/main/java/com/marosseleng/distancemeasurements/ui/about/AboutFragment.kt
+++ app/src/main/java/com/marosseleng/distancemeasurements/ui/about/AboutFragment.kt
@@ -1,10 +1,28 @@
+/*
+ * Copyright 2019 Maroš Šeleng
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.marosseleng.distancemeasurements.ui.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.marosseleng.distancemeasurements.R
 
 /**
@@ -13,6 +31,53 @@ import com.marosseleng.distancemeasurements.R
 class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+        val list = view.findViewById<RecyclerView>(R.id.dependenciesList)
+        list.adapter = DependenciesAdapter()
+        return view
+    }
+
+    class DependenciesAdapter : RecyclerView.Adapter<DependenciesAdapter.ViewHolder>() {
+
+        private val libraries = listOf(
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk7",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android",
+            "com.google.android.material:material",
+            "androidx.constraintlayout:constraintlayout",
+            "androidx.core:core-ktx",
+            "androidx.appcompat:appcompat",
+            "androidx.lifecycle:lifecycle-viewmodel-ktx",
+            "androidx.lifecycle:lifecycle-extensions",
+            "androidx.lifecycle:lifecycle-compiler",
+            "androidx.room:room-runtime",
+            "androidx.room:room-compiler",
+            "androidx.room:room-ktx",
+            "androidx.navigation:navigation-fragment-ktx",
+            "androidx.navigation:navigation-ui-ktx",
+            "com.jakewharton.timber:timber",
+            "com.github.PhilJay:MPAndroidChart",
+            "androidx.room:room-testing",
+            "junit:junit",
+            "androidx.test.ext:junit",
+            "androidx.test.espresso:espresso-core:3.1.1"
+        )
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+            )
+        }
+
+        override fun getItemCount() = libraries.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val library = libraries[position]
+            holder.text.text = library
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val text: TextView = itemView as TextView
+        }
     }
 }
--- app/src/main/java/com/marosseleng/distancemeasurements/ui/measurementdetail/MeasurementDetailFragment.kt
+++ app/src/main/java/com/marosseleng/distancemeasurements/ui/measurementdetail/MeasurementDetailFragment.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Maroš Šeleng
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.marosseleng.distancemeasurements.ui.measurementdetail
 
 import android.Manifest
@@ -12,7 +28,6 @@ import android.text.format.DateUtils.FORMAT_SHOW_TIME
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -67,7 +82,6 @@ class MeasurementDetailFragment : Fragment(), PositiveButtonClickedListener {
         viewModel = ViewModelProviders.of(this, MeasurementDetailViewModel.Factory(measurement))
             .get(MeasurementDetailViewModel::class.java)
 
-        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         title.text = measurement.timestamp.toString()
 
         valuesAdapter = MeasuredValueAdapter()
